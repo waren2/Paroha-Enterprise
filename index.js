09cee892d638f3570abce7e2a122aef9dfa8982d
waren2 <warenodhiambo2@gmail.com>
2025-05-03 09:19:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function menuClick(){
+    var x = document.getElementById("menuBar");
+    if(x.className === "topnav"){
+        x.className += "  responsive";
+    }else{
+        x.className = "topnav";
+    }
+}
